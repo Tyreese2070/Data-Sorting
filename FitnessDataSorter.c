27e3ce4f6ffb7e73456dd1a954053799d125276b
Tyreese2070 <146450901+Tyreese2070@users.noreply.github.com>
2023-11-16 13:45:28 +0000

    Added file for task3

--- FitnessDataSorter.c
+++ FitnessDataSorter.c
@@ -28,6 +28,40 @@ void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *s
 
 int main() 
 {
+    int count = 0;
+    FITNESS_DATA data_array[100];
+    char filename[25];
+    char date[11];
+    char time[6];
+    char steps[10];
+
+    int buffer_size = 1024;
+    char line_buffer[buffer_size];
     
+    printf("Enter filename: ");
+    scanf("%s", filename);
+
+    char data[25];
+    strcpy(data, filename);
+    FILE *file = fopen(data, "r");
+    if (file == NULL)
+    {
+        printf("Error: Could not open file\n");
+        return 1;
+    }
+    else
+    {
+        printf("File successfully loaded.\n");
+        while (fgets(line_buffer, buffer_size, file) != NULL)
+        {
+            tokeniseRecord(line_buffer, delimiter, date, time, steps);
+            strcpy(data_array[count].date, date);
+            strcpy(data_array[count].time, time);
+            data_array[count].steps = atoi(steps);
+            count ++;
+        }
+    }
+
+    return 0;
     
 }

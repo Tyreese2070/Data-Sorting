0c732b1fa5e45081ee829df7065674e4cf52303f
Tyreese2070 <146450901+Tyreese2070@users.noreply.github.com>
2024-01-29 11:43:31 +0000

    formatting

--- task1/StepsTask1.c
+++ task1/StepsTask1.c
@@ -3,47 +3,51 @@
 #include <string.h>
 
 // Define an appropriate struct
-typedef struct {
-	char date[11];
-	char time[6];
-	int steps;
+typedef struct
+{
+    char date[11];
+    char time[6];
+    int steps;
 } FITNESS_DATA;
 
 // Define any additional variables here
-
-
 
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
 // Ouputs: date character array; time character array; steps character array
 void tokeniseRecord(const char *input, const char *delimiter,
-                    char *date, char *time, char *steps) {
+                    char *date, char *time, char *steps)
+{
     // Create a copy of the input string as strtok modifies the string
     char *inputCopy = strdup(input);
-    
+
     // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
-    if (token != NULL) {        strcpy(date, token);
+    if (token != NULL)
+    {
+        strcpy(date, token);
     }
-    
+
     token = strtok(NULL, delimiter);
-    if (token != NULL) {
+    if (token != NULL)
+    {
         strcpy(time, token);
     }
-    
+
     token = strtok(NULL, delimiter);
-    if (token != NULL) {
+    if (token != NULL)
+    {
         strcpy(steps, token);
     }
-    
+
     // Free the duplicated string
     free(inputCopy);
-
 }
 
 // Complete the main function
-int main() {
-    
+int main()
+{
+
     // Opening the file
     char data[] = "FitnessData_2023.csv";
     FILE *file = fopen(data, "r");
@@ -72,7 +76,7 @@ int main() {
         strcpy(data_array[count].date, date);
         strcpy(data_array[count].time, time);
         data_array[count].steps = atoi(steps);
-        count ++;
+        count++;
     }
 
     printf("Number of records in file: %d\n", count);
--- task2/StepCounter.c
+++ task2/StepCounter.c
@@ -10,35 +10,37 @@ int datasize = 200;
 // Define any additional variables here
 // Global variables for filename and FITNESS_DATA array
 
-
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
 // Ouputs: date character array; time character array; steps character array
 void tokeniseRecord(const char *input, const char *delimiter,
-                    char *date, char *time, char *steps) {
+                    char *date, char *time, char *steps)
+{
     // Create a copy of the input string as strtok modifies the string
     char *inputCopy = strdup(input);
-    
+
     // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
-    if (token != NULL) {        strcpy(date, token);
+    if (token != NULL)
+    {
+        strcpy(date, token);
     }
-    
+
     token = strtok(NULL, delimiter);
-    if (token != NULL) {
+    if (token != NULL)
+    {
         strcpy(time, token);
     }
-    
+
     token = strtok(NULL, delimiter);
-    if (token != NULL) {
+    if (token != NULL)
+    {
         strcpy(steps, token);
     }
-    
+
     // Free the duplicated string
     free(inputCopy);
-
-                    }
-
+}
 
 void feweststeps(FITNESS_DATA data[datasize], int count)
 {
@@ -54,7 +56,7 @@ void feweststeps(FITNESS_DATA data[datasize], int count)
         }
     }
 
-    printf("Fewest steps: %s %s\n", data[leastrec].date, data[leastrec].time);	
+    printf("Fewest steps: %s %s\n", data[leastrec].date, data[leastrec].time);
 }
 
 void largeststeps(FITNESS_DATA data[datasize], int count)
@@ -71,27 +73,26 @@ void largeststeps(FITNESS_DATA data[datasize], int count)
         }
     }
     printf("Largest steps: %s %s\n", data[maxrecord].date, data[maxrecord].time);
-
 }
 
 void meansteps(FITNESS_DATA data[datasize], int count)
 {
-	float mean;
+    float mean;
     int total = 0;
 
-	for (int i = 0; i < count; i++)
-	{
+    for (int i = 0; i < count; i++)
+    {
         total += data[i].steps;
     }
-    mean = (float)total/count;
+    mean = (float)total / count;
     int intmean = (int)(mean + 0.5);
 
-    printf("Mean step count: %d\n", intmean);	
+    printf("Mean step count: %d\n", intmean);
 }
 
 void longestperiod(FITNESS_DATA data[datasize], int count)
 {
-	int startrecord = 0;
+    int startrecord = 0;
     int endrecord = 0;
     int tracking = 0;
     int longestnum = 0;
@@ -99,8 +100,8 @@ void longestperiod(FITNESS_DATA data[datasize], int count)
     int currentstart = 0;
     int currentmax = 0;
 
-	for (int i = 0; i < count; i++)
-	{
+    for (int i = 0; i < count; i++)
+    {
         if (data[i].steps > 500)
         {
             if (tracking == 0)
@@ -130,15 +131,14 @@ void longestperiod(FITNESS_DATA data[datasize], int count)
             tracking = 0;
             currentnum = 0;
         }
-        
     }
 
     printf("Longest period start: %s %s\n", data[startrecord].date, data[startrecord].time);
-    printf("Longest period end: %s %s\n", data[endrecord].date, data[endrecord].time);	
+    printf("Longest period end: %s %s\n", data[endrecord].date, data[endrecord].time);
 }
 
 // Complete the main function
-int main() 
+int main()
 {
     char userinput;
     char filename[50];
@@ -155,7 +155,7 @@ int main()
     int buffer_size = 1024;
     char line_buffer[buffer_size];
 
-    while(running == 1)
+    while (running == 1)
     {
         printf("A: Specify the filename to be imported\n");
         printf("B: Display the total number of records in the file\n");
@@ -166,93 +166,84 @@ int main()
         printf("Q: Quit\n");
         printf("Enter input choice: ");
         scanf(" %c", &userinput);
-    
-        switch(userinput)
+
+        switch (userinput)
         {
-            case 'A':
-            case 'a':  
-                printf("Input filename: ");
-                scanf("%s", filename);
-                char data[50];
-                strcpy(data, filename);
-                FILE *file = fopen(data, "r");
-                if (file == NULL)
+        case 'A':
+        case 'a':
+            printf("Input filename: ");
+            scanf("%s", filename);
+            char data[50];
+            strcpy(data, filename);
+            FILE *file = fopen(data, "r");
+            if (file == NULL)
+            {
+                printf("Error: Could not open file\n");
+                return 1;
+            }
+            else
+            {
+                fileopened = 1;
+                printf("File successfully loaded.\n");
+                while (fgets(line_buffer, buffer_size, file) != NULL)
                 {
-                    printf("Error: Could not open file\n");
-                    return 1;
+                    tokeniseRecord(line_buffer, delimiter, date, time, steps);
+                    strcpy(data_array[count].date, date);
+                    strcpy(data_array[count].time, time);
+                    data_array[count].steps = atoi(steps);
+                    count++;
                 }
-                else
-                {
-                    fileopened = 1;
-                    printf("File successfully loaded.\n");
-                    while (fgets(line_buffer, buffer_size, file) != NULL)
-                        {
-                            tokeniseRecord(line_buffer, delimiter, date, time, steps);
-                            strcpy(data_array[count].date, date);
-                            strcpy(data_array[count].time, time);
-                            data_array[count].steps = atoi(steps);
-                            count ++;
-                        }
-                }
-                fclose(file);
-                getchar();
+            }
+            fclose(file);
+            getchar();
 
-                /*
-                for (int i = 0; i < count; i++)
-                {
-                    printf("%s %s %d\n", data_array[i].date, data_array[i].time, data_array[i].steps);
-                }
-                */
+            /*
+            for (int i = 0; i < count; i++)
+            {
+                printf("%s %s %d\n", data_array[i].date, data_array[i].time, data_array[i].steps);
+            }
+            */
 
             break;
-            
-            case 'B':
-            case 'b': 
-                printf("Total records: %d\n", count); 
+
+        case 'B':
+        case 'b':
+            printf("Total records: %d\n", count);
             break;
 
-            case 'C':
-            case 'c':
-                feweststeps(data_array, count); 
+        case 'C':
+        case 'c':
+            feweststeps(data_array, count);
             break;
 
-            case 'D':
-            case 'd':
-                largeststeps(data_array, count);
-                
+        case 'D':
+        case 'd':
+            largeststeps(data_array, count);
+
             break;
 
-            case 'E':
-            case 'e':
-                meansteps(data_array, count);
-                
+        case 'E':
+        case 'e':
+            meansteps(data_array, count);
+
             break;
 
-            case 'F':
-            case 'f':
-                longestperiod(data_array, count);
+        case 'F':
+        case 'f':
+            longestperiod(data_array, count);
             break;
 
-            case 'Q':
-            case 'q': 
-                //return 0;
-                running = 0;
+        case 'Q':
+        case 'q':
+            // return 0;
+            running = 0;
             break;
-            
-            default: printf("Invalid choice. Try again.\n");
+
+        default:
+            printf("Invalid choice. Try again.\n");
             break;
         }
     }
 
     return 0;
-   
 }
-
-
-
-
-
-
-
-
-
